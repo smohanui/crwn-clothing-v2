4e77b12c093465e1ce42183bec07c77f2776355c
Administrator <Administrator@SUDHAMOHAN-LP>
2023-05-06 12:54:00 +0530

    Routes initiated, Sass introduced, Home route configured.

--- src/App.js
+++ src/App.js
@@ -1,42 +1,11 @@
+import { Route, Routes } from "react-router-dom";
+// import HomeComponent from "./Routes/Homes/Homes.component.jsx";
+import HomeComponent from "./Routes/Homes/Homes.component.jsx";
 const App = () => {
   return (
-    <div className='categories-container'>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Hats</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Jackets</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Sneakers</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Womens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Mens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-    </div>
+    <Routes>
+      <Route path="/home" index element={<HomeComponent />} />
+    </Routes>
   );
 };
 
